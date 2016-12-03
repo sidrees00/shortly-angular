05ec488b4a880e4eef07ccb90f91d6db4f133c35
sidrees00 <gusmanbhai@hotmail.com>
2016-12-02 19:46:19 -0800

    Pass tests, implement shorten & links controllers, and Links factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,27 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getAll = function() {
+    
+    return $http.get('/api/links')
+    .then(function(links) {
+      return links.data;
+    });
+  };
+  var addOne = function(link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+    .then(function(res) {
+      return res;
+    });
+  };
+  return {
+    getAll: getAll,
+    addOne: addOne
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
