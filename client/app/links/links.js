74b29cadb9322bb4f881c6311a832dc15a0fdbee
sidrees00 <gusmanbhai@hotmail.com>
2016-12-03 17:06:50 -0800

    Implement filter and validate functionality

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -4,6 +4,7 @@ var links = angular.module('shortly.links', []);
 
 links.controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.filter = '';
   $scope.data = {};
   Links.getAll().then(function(links) {
     $scope.data.links = links;
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,29 +1,15 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
   $scope.submitted = false;
   $scope.link = '';
 
-  $scope.validate = function(link) {
-    //validate
-      //if fails
-        //alert user
-      //if success
-        //$scope.addLink(link)
-
-  };
-  //
-
   $scope.addLink = function(link) {
     var prepLink = {url: link};
-    console.log('submitted link', link);
-    $scope.submitted = true;
     Links.addOne(prepLink).then(function(link) {
-      console.log(link, link.data, '*************');
       $scope.newLink = link.data;
+      $scope.submitted = true;
     });
     $scope.link = '';
   };
-  // $scope.renderSubmitted = 
 });
